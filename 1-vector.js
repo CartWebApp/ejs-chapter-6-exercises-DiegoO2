4affb5ffdd577c18d1a73812c567ee611ba5b0ce
DiegoO2 <diego.hidalgo23@student.cart.org>
2024-02-21 14:50:53 -0800

    AIKUTEHiuyasget

--- 1-vector.js
+++ 1-vector.js
@@ -1,4 +1,15 @@
 // Write a class Vec that represents a vector in two-dimensional space. It takes x and y parameters (numbers), which it should save to properties of the same name.
+class Vec {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+    plus(vector){
+        this.x += vector.x;
+        this.y += vector.y;
+    }
+}
+
 
 // Give the Vec prototype two methods, plus and minus, that take another vector as a parameter and return a new vector that has the sum or difference of the two vectors’ (this and the parameter) x and y values.
 
@@ -6,11 +17,12 @@
 
 // Your code here.
 
+console.log(new Vec(1,2));
 
 // Tests:
-console.log(new Vec(1, 2).plus(new Vec(2, 3)));
-// → Vec{x: 3, y: 5}
-console.log(new Vec(1, 2).minus(new Vec(2, 3)));
-// → Vec{x: -1, y: -1}
-console.log(new Vec(3, 4).length);
-// → 5
+// console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// // → Vec{x: 3, y: 5}
+// console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// // → Vec{x: -1, y: -1}
+// console.log(new Vec(3, 4).length);
+// // → 5
